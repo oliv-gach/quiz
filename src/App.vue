48e5792c3de2d5e5ab3893568e35f0dc1b0525fb
oliv-gach <fog@jarus.fr>
2020-02-14 15:54:13 +0100

    Ebauches des composants Theme et ThemeSelect

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <main id="app" class="container-fluid">
+    <div class="row vh-100">
+      <div class="col-lg-4 theme-bloc">
+        <ThemeSelect :listeTheme="listeTheme" @select-theme="onSelectTheme" />
+        <Theme :idTheme="idTheme" />
+      </div>
+      <div class="col-lg-8 quiz-bloc">QUIZ</div>
+    </div>
+  </main>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Theme from "./components/Theme.vue";
+import ThemeSelect from "./components/ThemeSelect.vue";
+import * as data from "../data/quiz";
 
 export default {
-  name: "App",
+  name: "app",
   components: {
-    HelloWorld
+    Theme,
+    ThemeSelect
+  },
+  data: function() {
+    return {
+      listeTheme: data.listeTheme,
+      idTheme: null
+    };
+  },
+  methods: {
+    onSelectTheme(idTheme) {
+      this.idTheme = idTheme;
+    }
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.theme-bloc {
+  padding-top: 1rem;
+  background-color: #aca0c2;
+}
+.quiz-bloc {
+  padding-top: 1rem;
 }
 </style>
